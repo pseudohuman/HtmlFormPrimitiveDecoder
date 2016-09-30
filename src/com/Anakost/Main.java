57b3a092c33a82c8282da4c3408298ab10ca07fe
Anatoly Kostiuk <dreamwatchersking@gmail.com>
2016-10-01 01:28:22 +0300

    Simple converter

---
+++ src/com/Anakost/Main.java
@@ -0,0 +1,25 @@
+package com.Anakost;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+public class Main {
+    static String html="DNS=%24-_.%2B+%D0%B0+%D0%B1+%D0%B2+%D0%B3+%D0%B4+%D0%B6+%D0%B7&field+1=%21*%27%28%29%2C&field+2=%3B%3F%3A%40%26%3D%2F&field+3=%25%5C%7B%7D%5B%5D%23%5E%7E%7C";
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        decodeString(html);
+    }
+    private static void decodeString(String s) throws UnsupportedEncodingException {
+
+        String[]hello=s.split("&");
+        HashMap<String,String> map=new HashMap<String,String>();
+        for (String string:hello){
+            String name = string.substring(0, string.indexOf("="));
+            String value = string.substring( string.indexOf("=")+1,string.length());
+            System.out.println();
+            map.put(name,URLDecoder.decode(value,"UTF-8"));
+        }
+        // write your code here
+    }
+}
