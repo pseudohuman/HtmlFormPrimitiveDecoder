e29a41effbf9cc6ed1934e5475fe2235c091c469
Anatoly Kostiuk <dreamwatchersking@gmail.com>
2016-10-01 15:23:21 +0300

    Undecoded name fixed

--- src/com/Anakost/Main.java
+++ src/com/Anakost/Main.java
@@ -8,9 +8,9 @@ import java.util.HashMap;
 public class Main {
     static String html="DNS=%24-_.%2B+%D0%B0+%D0%B1+%D0%B2+%D0%B3+%D0%B4+%D0%B6+%D0%B7&field+1=%21*%27%28%29%2C&field+2=%3B%3F%3A%40%26%3D%2F&field+3=%25%5C%7B%7D%5B%5D%23%5E%7E%7C";
     public static void main(String[] args) throws UnsupportedEncodingException {
-        decodeString(html);
+        HashMap<String,String> map=decodeString(html);
     }
-    private static void decodeString(String s) throws UnsupportedEncodingException {
+    private static HashMap<String,String> decodeString(String s) throws UnsupportedEncodingException {
 
         String[]hello=s.split("&");
         HashMap<String,String> map=new HashMap<String,String>();
@@ -18,8 +18,9 @@ public class Main {
             String name = string.substring(0, string.indexOf("="));
             String value = string.substring( string.indexOf("=")+1,string.length());
             System.out.println();
-            map.put(name,URLDecoder.decode(value,"UTF-8"));
+            map.put(URLDecoder.decode(name,"UTF-8"),URLDecoder.decode(value,"UTF-8"));
         }
         // write your code here
+        return map;
     }
 }
